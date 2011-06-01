23e85b114abec1e4681013f432e1d33d45246660
Amit Levy <aalevy@gmail.com>
2011-06-01 15:29:56 -0700

    Initial code from rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Breadcrumbs::Application.config.secret_token = '664027103e584f60af94626c128ce21cda05c65dbe92c8dd448cf49c5184fa2a158dc62eb97c0d0571f88e84fc6302f5c70161ee1808607fee769dfca48bc8b6'
