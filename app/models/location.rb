c957e66d89309c8f14905ab52b8a2393ef43e560
Amit Levy <aalevy@gmail.com>
2011-06-07 21:01:14 +0300

    Removed application.css and added virtual name to location.rb

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,11 @@
 class Location < ActiveRecord::Base
   has_many :checkins
+  
+  def name
+    if administrative_zone
+      "#{city} #{administrative_zone}, #{country}"
+    else
+      "#{city}, #{country}"
+    end
+  end
 end
