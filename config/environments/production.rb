8ad9bec5426b3d43f91b2693e5d1735c7ac8fdcb
Amit Aryeh Levy <amit@amitlevy.com>
2012-06-24 23:40:45 -0700

    Testing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,8 @@ Breadcrumbs::Application.configure do
   # config.logger = SyslogLogger.new
 
   # Use a different cache store in production
-  config.cache_store = :dalli_store
+  config.cache_store = :dalli_store, ENV["MEMCACHIER_SERVERS"],
+                      {:username => ENV["MEMCACHIER_USERNAME"], :password => ENV["MEMCACHIER_PASSWORD"]}
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
